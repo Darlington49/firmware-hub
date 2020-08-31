157bec382a7ec93db913a8852b8f6bd2f0dc2046
Ali Youcef <youce.31@gmail.com>
2020-08-31 21:42:11 +0100

    login middle ware

---
+++ Middleware/requireLogin.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const User = mongoose.model("User");
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET} = require('../keys');
+
+module.exports = (req,res,next,()=>{
+    const {authorization} = req.headers;
+    //authorization ==== Bearer shdgshdsgds(token)
+    if(!authorization){
+        return res.status(401).json({error:"you must be logged"});
+    }
+    const token = authorization.replace("Bearer ","");
+    jwt.verify(token,JWT_SECRET,(err,payload)=>{
+        if (err){
+            return res.status(401).json({error:"you must be logged"});
+        }
+
+        const {_id}=payload;
+        User.findById(_id).then(userdata=>{req.user = userdata})
+    })
+    next()
+
+})
