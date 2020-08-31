e3d99aacd1d1f10d84e2e44e6ba4bf84e5d1b59f
Ali Youcef <youce.31@gmail.com>
2020-08-31 14:31:34 +0100

    configure data base localy at instaclone

--- keys.js
+++ keys.js
@@ -1,2 +1,2 @@
 module.exports={MONGOURI : "mongodb+srv://admin:1UzYrmV9ZlRTDHvG@cluster0.poywj.mongodb.net/sample_training?retryWrites=true&w=majority"}
-module.exports={MONGOURI : "mongodb://localhost:27017/mydatatest"}
+module.exports={MONGOURI : "mongodb://localhost:27017/instaclone"}
