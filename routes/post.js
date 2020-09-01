39067fcf982a4cfca53b7972eb0757e513b13505
Ali Youcef <youce.31@gmail.com>
2020-09-01 07:23:36 +0100

    add routh schema routh/post.js

---
+++ routes/post.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const requireLogin = require('../Middleware/requireLogin');
+// const post = mongoose.model("Post");
+require('./Models/user');
+router.post('createpost', requireLogin, (req, res) => {
+    const {
+        title,
+        body
+    } = req.body;
+    if (!title || !body) {
+        return res.status(422).json({
+            error: "please add all the fields"
+        });
+    }
+    const post = new post({
+        title,
+        body,
+        postedBy: req.user
+    });
+    post.save().then(result => {
+            res.json({
+                post: result
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+});
+module.exports = router;
