633ef7de3c8b091eb1806bb61ed8ff177c8ffec6
Ali Youcef <youce.31@gmail.com>
2020-09-01 06:59:47 +0100

    add code tc.js

---
+++ temp_codes.js
@@ -0,0 +1,12 @@
+// const Cat = mongoose.model('Cat', { name: String });
+
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
+
+// const customMiddleware = (req, res, next) => {
+//     console.log("Middleware Executed");
+//     // console.log(req);
+//     next();
+// }
+
+// app.use(customMiddleware);
