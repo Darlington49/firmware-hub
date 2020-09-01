66a2464db78aaa73ffe67de7f6ea1e5f1d771714
Ali Youcef <youce.31@gmail.com>
2020-09-01 07:25:50 +0100

    fix post routh.js

--- routes/post.js
+++ routes/post.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const requireLogin = require('../Middleware/requireLogin');
-// const post = mongoose.model("Post");
-require('./Models/user');
+
 router.post('createpost', requireLogin, (req, res) => {
     const {
         title,
