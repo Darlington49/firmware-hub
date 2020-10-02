3a43c732b6e57535257f5671ab07d4df3b88ec08
Ali Youcef <youce.31@gmail.com>
2020-10-02 16:27:35 +0100

    cred dbb

--- keys.js
+++ keys.js
@@ -1,5 +1,11 @@
 // module.exports={MONGOURI : "mongodb+srv://admin:1UzYrmV9ZlRTDHvG@cluster0.poywj.mongodb.net/sample_training?retryWrites=true&w=majority"}
 module.exports = {
-    MONGOURI: "mongodb://localhost:27017/instaclone",
+    // MONGOURI: "mongodb://localhost:27017/instaclone",
+    MONGOURI: "mongodb+srv://Youcef:azerty2030@cluster0.l6rym.gcp.mongodb.net/firmware-hub?retryWrites=true&w=majority",
     JWT_SECRET: "mmmnkjnuuh"
 }
+
+// username : Heroku
+// password : aY1S4KgrQvr6fTUr
+// dbname : firmware-hub
+// mongodb+srv://<username>:<password>@cluster0.l6rym.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority
